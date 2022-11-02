14a4005eaeb8313357b9cb01e1605827c45883bd
kazuya.zama <zama@restarts.co.jp>
2022-11-02 23:57:15 +0900

    errorfix

--- components/campaign/Detail/ListCpn.tsx
+++ components/campaign/Detail/ListCpn.tsx
@@ -1,28 +1,27 @@
 type Props = {
     title:string; 
+    discription?:string;
     list1:string;
-    list2:string | Array<string|JSX.Element>;
-    list3:string | Array<string|JSX.Element>;
-
+    list2?:string | Array<string|JSX.Element>;
+    list3?:string | Array<string|JSX.Element>;
+    others?:string;
 }
 
-const ListCpn = ({title,list1,list2,list3}:Props) => {
+const ListCpn = ({title,list1,list2,list3,discription,others}:Props) => {
     return (
         <div className="col-span-6 justify-self-start w-full">
         <h2 className="border-b-2 border-secondary "><span className="text-lg md:text-xl">{title}</span></h2>
            <hr />
+           {discription &&
+            ( <p className="py-3">{discription}</p> ) 
+           }
            <ul className="list-disc list-inside space-y-2 pt-3 ">
                <li>
                {list1}
                </li>
-               <li>
-               {list2}
-               </li>
-               <li>
-               {list3}
-               </li>
-             
-       
+               {list2 && (<li>{list2}</li> ) }  
+               {list3 && (<li>{list3}</li> ) }
+               {others && (<p className="py-2">{others}</p> )}  
            </ul>
         </div>
     );
